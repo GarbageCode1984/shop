924bee4bd68a6533431299cb6197f698ca73ec48
GarbageCode1984 <rkqlwl1984@gmail.com>
2023-08-13 00:24:20 +0900

    toast add

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,10 +5,18 @@ import "./App.css";
 import RegisterPage from "./pages/RegisterPage/index";
 import Navbar from "./layout/Navbar";
 import Footer from "./layout/Footer/index";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function Layout() {
     return (
         <div className="flex flex-col h-screen justify-between">
+            <ToastContainer
+                position="bottom-right"
+                theme="light"
+                pauseOnHover
+                autoClose={1500}
+            />
             <Navbar />
             <main className="mb-auto w-10/12 max-w-4xl mx-auto">
                 <Outlet />
