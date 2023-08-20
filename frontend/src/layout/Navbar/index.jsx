893446552045e18853e439b9ccb07aa1e4268210
GarbageCode1984 <rkqlwl1984@gmail.com>
2023-08-20 13:14:41 +0900

    navbar 추가중

--- frontend/src/layout/Navbar/index.jsx
+++ frontend/src/layout/Navbar/index.jsx
@@ -1,5 +1,31 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
-    return <div>Navbar</div>;
+    const [menu, setMenu] = useState(false);
+
+    const handleMenu = () => {
+        setMenu(!menu);
+    };
+
+    return (
+        <nav>
+            <div>
+                <div>
+                    <div>
+                        <Link to="/">Logo</Link>
+                    </div>
+
+                    <div>
+                        <button onClick={handleMenu}>{menu ? "-" : "+"}</button>
+                    </div>
+                </div>
+
+                {/* mobile nav-items */}
+                <div></div>
+            </div>
+        </nav>
+    );
 };
 
 export default Navbar;
