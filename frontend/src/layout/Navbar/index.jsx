725030d6e81eea4799acb4f4fb9c0393ed11d106
GarbageCode1984 <rkqlwl1984@gmail.com>
2023-08-21 02:18:39 +0900

    로그아웃 기능 구현 완료

--- frontend/src/layout/Navbar/index.jsx
+++ frontend/src/layout/Navbar/index.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import NavItem from "./Sections/NavItem";
 
 const Navbar = () => {
     const [menu, setMenu] = useState(false);
@@ -9,20 +10,27 @@ const Navbar = () => {
     };
 
     return (
-        <nav>
-            <div>
-                <div>
-                    <div>
+        <nav className="relative z-10 text-white bg-gray-900">
+            <div className="w-full">
+                <div className="flex items-center justify-between mx-5 sm:mx-10 lg:mx-20">
+                    <div className="flex items-center text-2xl h-14">
                         <Link to="/">Logo</Link>
                     </div>
 
-                    <div>
+                    <div className="text-2xl sm:hidden">
                         <button onClick={handleMenu}>{menu ? "-" : "+"}</button>
+                    </div>
+
+                    {/* big screen nav-items */}
+                    <div className="hidden sm:block">
+                        <NavItem />
                     </div>
                 </div>
 
                 {/* mobile nav-items */}
-                <div></div>
+                <div className="block sm:hidden">
+                    {menu && <NavItem mobile />}
+                </div>
             </div>
         </nav>
     );
